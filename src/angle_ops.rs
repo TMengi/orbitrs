efb19d57412cdc7688518bdc6740e6ab1e667e6d
tyler <tmccown21@gmail.com>
2024-09-06 21:12:54 -0700

    More angle wrapping

--- src/angle_ops.rs
+++ src/angle_ops.rs
@@ -1,11 +1,19 @@
-use std::f64;
+use std::f64::{self, consts::PI};
 
-const TWO_PI: f64 = 2.0 * f64::consts::PI;
+const TWO_PI: f64 = 2.0 * PI;
 
 pub fn wrap_0_2pi(angle: f64) -> f64 {
     let mut wrapped_angle = angle % TWO_PI;
     if wrapped_angle < 0.0 {
         wrapped_angle += TWO_PI;
+    }
+    wrapped_angle
+}
+
+pub fn wrap_negpi_pi(angle: f64) -> f64 {
+    let mut wrapped_angle = wrap_0_2pi(angle);
+    if wrapped_angle > PI {
+        wrapped_angle -= TWO_PI;
     }
     wrapped_angle
 }
@@ -37,6 +45,37 @@ mod wrap_0_2pi_tests {
         let expected_angle = 11.0 * f64::consts::FRAC_PI_6;
         let angle = expected_angle - 2.0 * TWO_PI;
         let wrapped_angle = wrap_0_2pi(angle);
+        assert_relative_eq!(wrapped_angle, expected_angle, epsilon = 10.0 * f64::EPSILON);
+    }
+}
+
+#[cfg(test)]
+mod wrap_negpi_pi_tests {
+    use super::*;
+
+    #[test]
+    /// Angles already on the domain [-pi, pi] are unaffected
+    fn test_no_wrapping() {
+        let angle = f64::consts::FRAC_PI_6;
+        let wrapped_angle = wrap_negpi_pi(angle);
+        assert_relative_eq!(wrapped_angle, angle);
+    }
+
+    #[test]
+    /// Positive angles greater than pi get wrapped
+    fn test_positive_wrapping() {
+        let angle = 7.0 * f64::consts::FRAC_PI_6;
+        let expected_angle = -5.0 * f64::consts::FRAC_PI_6;
+        let wrapped_angle = wrap_negpi_pi(angle);
+        assert_relative_eq!(wrapped_angle, expected_angle, epsilon = 10.0 * f64::EPSILON);
+    }
+
+    #[test]
+    /// Negative angles less than -pi get wrapped
+    fn test_negative_wrapping() {
+        let angle = -7.0 * f64::consts::FRAC_PI_6;
+        let expected_angle = 5.0 * f64::consts::FRAC_PI_6;
+        let wrapped_angle = wrap_negpi_pi(angle);
         assert_relative_eq!(wrapped_angle, expected_angle, epsilon = 10.0 * f64::EPSILON);
     }
 }
