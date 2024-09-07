dfbea546a6205cf4273da987ed47498efab869c0
tyler <tmccown21@gmail.com>
2024-09-06 21:57:32 -0700

    Improve array equality assertion message

--- src/testing.rs
+++ src/testing.rs
@@ -1,5 +1,11 @@
 pub fn assert_array_eq(a: &[f64], b: &[f64]) {
-    for (elem_a, elem_b) in a.into_iter().zip(b.into_iter()) {
-        assert_relative_eq!(elem_a, elem_b);
+    let mut success = true;
+    for (elem_a, elem_b) in a.iter().zip(b.iter()) {
+        success &= relative_eq!(elem_a, elem_b);
+    }
+    if !success {
+        // let msg = format!("{:?}, {:?}", a, b);
+        // panic!(msg)
+        panic!("\tleft  = {:?}\n\tright = {:?}", a, b);
     }
 }
