c5b84f5906a534c95c7318de3b660e4cdaf00963
tyler <tmccown21@gmail.com>
2024-09-07 20:59:20 -0700

    Assert array equality with a given tolerance

--- src/testing.rs
+++ src/testing.rs
@@ -1,11 +1,27 @@
+use std::f64;
+
+// TODO: Replace this with a macro so we don't need multiple versions
 pub fn assert_array_eq(a: &[f64], b: &[f64]) {
+    assert_array_eq_atol(a, b, f64::EPSILON);
+}
+
+pub fn assert_array_eq_atol(a: &[f64], b: &[f64], atol: f64) {
     let success = a
         .iter()
         .zip(b.iter())
-        .map(|(elem_a, elem_b)| relative_eq!(elem_a, elem_b))
+        .map(|(elem_a, elem_b)| relative_eq!(elem_a, elem_b, epsilon = atol))
         .reduce(|acc, e| acc && e)
         .unwrap_or(false);
     if !success {
-        panic!("\tleft  = {:?}\n\tright = {:?}", a, b);
+        let max_diff: f64 = a
+            .iter()
+            .zip(b.iter())
+            .map(|(elem_a, elem_b)| f64::abs(elem_a - elem_b))
+            .fold(0.0, f64::max);
+
+        panic!(
+            "\tleft  = {:?}\n\tright = {:?}\n\tmax adiff = {:.3e}\n\ttol = {:.3e}",
+            a, b, max_diff, atol
+        );
     }
 }
